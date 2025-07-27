51e1184d7755fbd643de6d81879051ce1f9aab6f
Rob S <Robb.Stumpf@GMail.com>
2025-07-27 18:45:39 -0400

    Add files via upload
    0.7.0 Release - EFT viewer and editor support has arrived!

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
+app_name = "viewer"
 urlpatterns = [
     path('view', views.viewer, name='viewer'),
+    path('save_edited_eft/', views.save_edited_eft, name='save_edited_eft'),
 ]

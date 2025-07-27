c819e07e0b58c42ffed102585e20e5849bb399ee
Rob S <Robb.Stumpf@GMail.com>
2025-07-27 18:43:38 -0400

    Add files via upload
    0.7.0 Release - Added additional support for EFT viewer URLs

--- OpenEFT/urls.py
+++ OpenEFT/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('conversion.urls')),
     path('', include('about.urls')),
-    path('', include('viewer.urls')),
+    path('', include(('viewer.urls', 'viewer'), namespace='viewer')),
     path('admin/', admin.site.urls),
 ]
